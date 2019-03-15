d7875ed55c8811a54418da61f706cd9aaf67aab0
chibanimaher <chibanimaher@gmail.com>
2019-03-15 14:20:08 +0100

    first Commit

---
+++ boygImmo/src/test/java/config/Config.java
@@ -0,0 +1,23 @@
+package config;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class Config {
+	public WebDriver driver;
+    public WebDriverWait wait;
+	@BeforeTest
+	public void setUp() throws Exception {
+		driver = new ChromeDriver();
+		wait = new WebDriverWait(driver, 20);
+	}
+
+	@AfterTest
+	public void tearDown() throws Exception {
+		 driver.close();
+
+	}
+}
---
+++ boygImmo/src/test/java/config/RunTest.java
@@ -0,0 +1,17 @@
+package config;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
+
+public class RunTest extends Config {
+@Test
+public void test() throws Exception {
+	driver.get("https://www.bouygues-immobilier.com/");
+	driver.manage().window().maximize();
+Thread.sleep(2000);
+   // 
+  wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#dossier-client"))).click();
+    Thread.sleep(2000);
+}
+}
