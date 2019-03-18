ca95bdd6b402ba717ea2f03d2021e035733d53b2
chibanimaher <chibanimaher@gmail.com>
2019-03-18 15:43:32 +0100

    seondcommit

--- boygImmo/src/test/java/config/RunTest.java
+++ boygImmo/src/test/java/config/RunTest.java
@@ -10,7 +10,7 @@ public void test() throws Exception {
 	driver.get("https://www.bouygues-immobilier.com/");
 	driver.manage().window().maximize();
 Thread.sleep(2000);
-   // hdzaudzadazdxadgdfgdfdf
+   // hdzaudzadazdxadgdfgdfdfdfsdfdsfsdf
   wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#dossier-client"))).click();
     Thread.sleep(2000);
     //dsjkdcdddhzeuhduezbczuic
